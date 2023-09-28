7db81c7f49d6c764b599c026c533221e27f4ba49
sudoicoder <sudoicoder@gmail.com>
2023-09-28 19:54:56 +0530

    CapitalsGame: created generateUnmatchedList

--- src/components/CapitalsGame.tsx
+++ src/components/CapitalsGame.tsx
@@ -2,6 +2,10 @@ export default function CapitalsGame({}: CapitalsGameProps) {
   return <div>CapitalsGame</div>
 }
 
+function generatedUnmatchedList(data: CapitalsGameProps["data"]) {
+  return Object.entries(data).flat()
+}
+
 interface CapitalsGameProps {
   data: Record<Country, Capital>
 }
