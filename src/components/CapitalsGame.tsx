735eed238510db4e0ce77584d0612f052e6d2387
sudoicoder <sudoicoder@gmail.com>
2023-09-28 19:59:57 +0530

    CapitalsGame: created Unmatched type and implemented generateUnmatchedList

--- src/components/CapitalsGame.tsx
+++ src/components/CapitalsGame.tsx
@@ -2,13 +2,26 @@ export default function CapitalsGame({}: CapitalsGameProps) {
   return <div>CapitalsGame</div>
 }
 
-function generatedUnmatchedList(data: CapitalsGameProps["data"]) {
-  return Object.entries(data).flat()
-}
-
 interface CapitalsGameProps {
   data: Record<Country, Capital>
 }
+
+function generatedUnmatchedList(data: CapitalsGameProps["data"]): Unmatched[] {
+  return Object.entries(data).flatMap(([country, capital]) => [
+    { kind: "country", name: country },
+    { kind: "capital", name: capital },
+  ])
+}
+
+type Unmatched =
+  | {
+      kind: "country"
+      name: Country
+    }
+  | {
+      kind: "capital"
+      name: Capital
+    }
 
 type Country = string
 type Capital = string
