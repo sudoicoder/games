09aedc7128f3a35e482420ebb47f3d8e97c9f885
sudoicoder <sudoicoder@gmail.com>
2023-09-28 22:13:11 +0530

    App: restored to return App

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,3 @@
-import CapitalsGame from "@/components/CapitalsGame"
-
 export default function App() {
-  return (
-    <div>
-      <CapitalsGame
-        data={{
-          Germany: "Berlin",
-          India: "New Delhi",
-          Turkiye: "Ankara",
-          Azerbaijan: "Baku",
-        }}
-      />
-    </div>
-  )
+  return <div>App</div>
 }
