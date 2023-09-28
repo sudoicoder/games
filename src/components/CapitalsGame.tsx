c30037f01de762675dc516c45bb89792e724beab
sudoicoder <sudoicoder@gmail.com>
2023-09-28 19:50:20 +0530

    CapitalsGame: added data prop

--- src/components/CapitalsGame.tsx
+++ src/components/CapitalsGame.tsx
@@ -2,4 +2,9 @@ export default function CapitalsGame({}: CapitalsGameProps) {
   return <div>CapitalsGame</div>
 }
 
-interface CapitalsGameProps {}
+interface CapitalsGameProps {
+  data: Record<Country, Capital>
+}
+
+type Country = string
+type Capital = string
