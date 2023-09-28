238baaf91b59864b41a007ef50e35e9905885d63
sudoicoder <sudoicoder@gmail.com>
2023-09-28 21:44:00 +0530

    CapitalsGame: Done

--- src/components/CapitalsGame.tsx
+++ src/components/CapitalsGame.tsx
@@ -3,15 +3,33 @@ import { useState } from "react"
 import classes from "@/styles/capitalsgame.module.css"
 
 export default function CapitalsGame({ data }: CapitalsGameProps) {
-  const [unmatcheds] = useState(() => generateUnmatchedList(data))
-  const [selected] = useState<Unmatched[]>([])
-  function handleClick(unmatched: Unmatched) {}
+  const [unmatcheds, setUnmatched] = useState(() => generateUnmatchedList(data))
+  const [clicked, setClicked] = useState<Unmatched[]>([])
+  function handleClick(unmatched: Unmatched) {
+    if (clicked.length !== 1) {
+      setClicked([unmatched])
+      return
+    }
+    const previous = clicked[0]
+    if (previous.kind === unmatched.kind) {
+      setClicked([previous, unmatched])
+      return
+    }
+    const country = previous.kind === "country" ? previous.name : unmatched.name
+    const capital = previous.kind === "capital" ? previous.name : unmatched.name
+    if (data[country] !== capital) {
+      setClicked([previous, unmatched])
+      return
+    }
+    setClicked([])
+    setUnmatched(ums => ums.filter(um => um !== previous && um !== unmatched))
+  }
   return (
     <div className={classes.game}>
       {unmatcheds.map(unmatched => {
-        const extraClasses = !selected.includes(unmatched)
+        const extraClasses = !clicked.includes(unmatched)
           ? null
-          : selected.length < 2
+          : clicked.length < 2
           ? classes["tile-selected"]
           : classes["tile-mismatched"]
         return (
