b8101ec516b1145093d80852e40186f5081b901b
sudoicoder <sudoicoder@gmail.com>
2023-09-29 19:28:07 +0530

    created seperate funcs

--- src/chess/services/getPossibleMoves.ts
+++ src/chess/services/getPossibleMoves.ts
@@ -1,10 +1,80 @@
-import type { Board, Category } from "./createBoard"
+import AssertUnreachableError from "@/utils/AssertUnreachableError"
+
+import empty from "../utils/empty"
+
+import type { Board } from "./createBoard"
 
 export type PossibleMove = [number, number]
 
 export default function getPossibleMoves(
   board: Board,
-  category: Category,
+  row: number,
+  col: number
+): PossibleMove[] {
+  const piece = board[row][col]
+  if (piece === empty) {
+    return []
+  }
+  switch (piece.category) {
+    case "bishop":
+      return getBishopPossibleMoves(board, row, col)
+    case "king":
+      return getKingPossibleMoves(board, row, col)
+    case "knight":
+      return getKnightPossibleMoves(board, row, col)
+    case "pawn":
+      return getPawnPossibleMoves(board, row, col)
+    case "queen":
+      return getQueenPossibleMoves(board, row, col)
+    case "rook":
+      return getRookPossibleMoves(board, row, col)
+    default:
+      throw new AssertUnreachableError(piece.category)
+  }
+}
+
+function getBishopPossibleMoves(
+  board: Board,
+  row: number,
+  col: number
+): PossibleMove[] {
+  return []
+}
+
+function getKingPossibleMoves(
+  board: Board,
+  row: number,
+  col: number
+): PossibleMove[] {
+  return []
+}
+
+function getKnightPossibleMoves(
+  board: Board,
+  row: number,
+  col: number
+): PossibleMove[] {
+  return []
+}
+
+function getPawnPossibleMoves(
+  board: Board,
+  row: number,
+  col: number
+): PossibleMove[] {
+  return []
+}
+
+function getQueenPossibleMoves(
+  board: Board,
+  row: number,
+  col: number
+): PossibleMove[] {
+  return []
+}
+
+function getRookPossibleMoves(
+  board: Board,
   row: number,
   col: number
 ): PossibleMove[] {
