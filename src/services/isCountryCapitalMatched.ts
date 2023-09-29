4e34eb8ff4602e363bca031097f07df90f112b23
sudoicoder <sudoicoder@gmail.com>
2023-09-29 10:23:15 +0530

    isCountryCapitalMatched: renamed const record

--- src/services/isCountryCapitalMatched.ts
+++ src/services/isCountryCapitalMatched.ts
@@ -1,7 +1,7 @@
 import getCountryCapitalRecord from "./getCountryCapitalRecord"
 
-const record = getCountryCapitalRecord()
+const RECORD = getCountryCapitalRecord()
 
 export default function isCountryCapitalMatched(x: string, y: string) {
-  return record[x] === y || record[y] === x
+  return RECORD[x] === y || RECORD[y] === x
 }
