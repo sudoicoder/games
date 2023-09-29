34e35e4901dd77e3eefdcdfb7d41f15ef4c54329
sudoicoder <sudoicoder@gmail.com>
2023-09-29 10:26:19 +0530

    App: using modified CountryCapitalGame

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,9 @@
 import CountryCapitalGame from "@/components/CountryCapitalGame"
-import getCountryCapitalRecord from "@/services/getCountryCapitalRecord"
 
 export default function App() {
   return (
     <div>
-      <CountryCapitalGame data={getCountryCapitalRecord()} />
+      <CountryCapitalGame />
     </div>
   )
 }
