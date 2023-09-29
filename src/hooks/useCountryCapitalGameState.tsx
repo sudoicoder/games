1779accceb1d7bd13109c777fa4518a707fba088
sudoicoder <sudoicoder@gmail.com>
2023-09-29 10:23:43 +0530

    useCountryCapitalGameState: created

---
+++ src/hooks/useCountryCapitalGameState.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+import getRandomizedCountryCapitalList from "@/services/getRandomizedCountryCapitalList"
+import isCountryCapitalMatched from "@/services/isCountryCapitalMatched"
+
+export default function useCountryCapitalGameState() {
+  const [unmatcheds, setUnmatched] = useState(getRandomizedCountryCapitalList)
+  const [clicked, setClicked] = useState<string[]>([])
+
+  function getPhase(value: string) {
+    if (clicked.includes(value)) {
+      return clicked.length < 2 ? "selected" : "mismatched"
+    }
+    return "default"
+  }
+
+  function handleClick(current: string) {
+    if (clicked.length !== 1) {
+      setClicked([current])
+      return
+    }
+    const previous = clicked[0]
+    if (previous === current) {
+      return
+    }
+    if (isCountryCapitalMatched(previous, current)) {
+      setClicked([previous, current])
+      return
+    }
+    setClicked([])
+    setUnmatched(ums => ums.filter(um => um !== previous && um !== current))
+  }
+
+  function restartGame() {
+    setUnmatched(getRandomizedCountryCapitalList)
+  }
+
+  return { getPhase, handleClick, restartGame, unmatcheds }
+}
