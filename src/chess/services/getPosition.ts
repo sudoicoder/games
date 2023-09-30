ab604a9c797edc39b3301dbc68b91e94b4b952c8
sudoicoder <sudoicoder@gmail.com>
2023-09-30 17:13:45 +0530

    getPosition: type anoted

--- src/chess/services/getPosition.ts
+++ src/chess/services/getPosition.ts
@@ -5,11 +5,11 @@ const POSITIONS = Array(8)
   .map((_, row) => {
     return Array(8)
       .fill(null)
-      .map((_, col) => {
+      .map((_, col): Position => {
         return [row, col]
       })
   })
 
-export default function getPosition(row: number, col: number) {
+export default function getPosition(row: number, col: number): Position {
   return POSITIONS[row][col]
 }
