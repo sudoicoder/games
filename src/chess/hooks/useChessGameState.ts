9e5467d076a5e3b6b5f41de2935cfc0e19a987b1
sudoicoder <sudoicoder@gmail.com>
2023-09-30 17:30:15 +0530

    useChessGameState: implemented handle click and extracted small hooks

--- src/chess/hooks/useChessGameState.ts
+++ src/chess/hooks/useChessGameState.ts
@@ -1,49 +1,117 @@
-import { useRef, useState } from "react"
+import { useMemo, useRef, useState } from "react"
 
-import type { Piece } from "../services/createBoard"
-import type { ColorVariant } from "../services/getColorNotation"
-import type { PossibleMove } from "../services/getPossibleMoves"
+import type { AllianceType } from "../services/getAlliance"
+import type { Position } from "../services/getPosition"
 
-import createBoard from "../services/createBoard"
-import getEmptySquare from "../services/getEmptySquare"
+import createBoard, { Board } from "../services/createBoard"
+import getAlliance from "../services/getAlliance"
+import getPosition from "../services/getPosition"
 import getPossibleMoves from "../services/getPossibleMoves"
 
 export default function useChessGameState() {
-  const [board] = useState(createBoard)
-  const [selected, setSelected] = useState<Piece | null>(null)
-  const [possibleMoves, setPossibleMoves] = useState<PossibleMove[]>([])
+  const { board, move, capture } = useBoard()
+  const { selectedPosition, select, deselect } = useSelectedPosition()
+  const { turn, flip } = useTurn()
 
-  const turn = useRef<ColorVariant>("light")
+  const { possibleMoves } = usePossibleMoves(board, selectedPosition)
 
-  function getSquarePhase(row: number, col: number) {
-    const piece = board[row][col]
-    if (selected === piece) {
-      return "selected"
-    }
-    if (!possibleMoves.some(([r, c]) => r === row && c === col)) {
+  function getPhase(row: number, col: number) {
+    if (selectedPosition === undefined) {
       return "default"
     }
-    if (piece !== getEmptySquare()) {
-      return "capturable"
+    const position = getPosition(row, col)
+    if (selectedPosition === position) {
+      return "selected"
     }
-    return "possible"
+    if (possibleMoves.includes(position)) {
+      return board[row][col] !== null ? "capturable" : "possible"
+    }
+    return "default"
   }
 
   function handleClick(row: number, col: number) {
+    const position = getPosition(row, col)
     const piece = board[row][col]
-    if (piece === getEmptySquare()) {
+    if (selectedPosition === undefined) {
+      if (piece !== null) {
+        if (piece.includes(getAlliance(turn))) {
+          select(position)
+        }
+      }
       return
     }
-    if (selected === piece) {
-      setSelected(null)
+    if (selectedPosition === position) {
+      return void deselect()
+    }
+    if (!possibleMoves.includes(position)) {
+      return void deselect()
+    }
+    if (piece === null) {
+      move(selectedPosition, position)
+      flip()
       return
     }
-    if (piece.includes(turn.current)) {
-      setSelected(piece)
-      setPossibleMoves(getPossibleMoves())
-      return
+    capture(selectedPosition, position)
+    flip()
+    return
+  }
+
+  return { board, getPhase, handleClick } as const
+}
+
+function useBoard() {
+  const [board] = useState(createBoard)
+
+  function move(from: Position, to: Position) {
+    board[to[0]][to[1]] = board[from[0]][from[1]]
+    board[from[0]][from[1]] = null
+  }
+
+  function capture(from: Position, to: Position) {
+    const capturing = board[to[0]][to[1]]
+    move(from, to)
+    return capturing
+  }
+
+  return { board, move, capture } as const
+}
+
+function useSelectedPosition() {
+  const [selectedPosition, setSelectedPosition] = useState<Position>()
+
+  function select(position: Position) {
+    return void setSelectedPosition(position)
+  }
+
+  function deselect() {
+    return void setSelectedPosition(undefined)
+  }
+
+  return { selectedPosition, select, deselect } as const
+}
+
+function usePossibleMoves(
+  board: Board,
+  selectedPosition: Position | undefined
+) {
+  const possibleMoves = useMemo(
+    () => (selectedPosition ? getPossibleMoves(board, selectedPosition) : []),
+    [board, selectedPosition]
+  )
+  return { possibleMoves } as const
+}
+
+function useTurn() {
+  const turn = useRef<AllianceType>("Light")
+
+  function flip() {
+    if (turn.current === "Light") {
+      turn.current = "Dark"
+    }
+    if (turn.current === "Dark") {
+      turn.current = "Light"
     }
   }
 
-  return { board, getSquarePhase, handleClick } as const
+  return { turn: turn.current, flip } as const
 }
