6cd658141f1edf8171c1342e9f2eeccbba5cd0f8
sudoicoder <sudoicoder@gmail.com>
2023-09-30 12:38:22 +0530

    Deleted getEmptySquare

--- src/chess/services/getEmptySquare.ts
+++
@@ -1,7 +0,0 @@
-const EMPTY_SQUARE = Symbol("chess/board/square/empty")
-
-export default function getEmptySquare(): EmptySquare {
-  return EMPTY_SQUARE
-}
-
-export type EmptySquare = typeof EMPTY_SQUARE
