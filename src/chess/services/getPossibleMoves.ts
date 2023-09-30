f9a24c8632d686b4e6d248f8e10e70abef6d5db8
sudoicoder <sudoicoder@gmail.com>
2023-09-30 16:03:58 +0530

    getPossibleMoves: coded simple moves

--- src/chess/services/getPossibleMoves.ts
+++ src/chess/services/getPossibleMoves.ts
@@ -3,14 +3,145 @@ import type { Alliance } from "./getAlliance"
 import type { Position } from "./getOffsetedPosition"
 import type { Direction } from "./getStepsAlong"
 
+import getAlliance from "./getAlliance"
 import getOffsetedPosition from "./getOffsetedPosition"
+import getPieceAlliance from "./getPieceAlliance"
+import getPieceNotation from "./getPieceNotation"
 import getStepsAlong from "./getStepsAlong"
 
 export default function getPossibleMoves(
   board: Board,
   position: Position
 ): PossibleMove[] {
+  const piece = board[position[0]][position[1]]
+  if (piece === null) {
+    return []
+  }
+  const alliance = getPieceAlliance(piece)
+  if (piece.includes(getPieceNotation("Bishop"))) {
+    return getPossibleMovesForBishop(board, position, alliance)
+  }
+  if (piece.includes(getPieceNotation("Rook"))) {
+    return getPossibleMovesForRook(board, position, alliance)
+  }
+  if (piece.includes(getPieceNotation("Knight"))) {
+    return getPossibleMovesForKnight(board, position, alliance)
+  }
+  if (piece.includes(getPieceNotation("Queen"))) {
+    return getPossibleMovesForQueen(board, position, alliance)
+  }
+  if (piece.includes(getPieceNotation("King"))) {
+    return getPossibleMovesForKing(board, position, alliance)
+  }
+  if (piece.includes(getPieceNotation("Pawn"))) {
+    return getPossibleMovesForPawn(board, position, alliance)
+  }
   return []
+}
+
+function getPossibleMovesForPawn(
+  board: Board,
+  position: Position,
+  alliance: Alliance
+) {
+  const isDark = alliance === getAlliance("Dark")
+  const isIdle = isDark ? position[0] === 1 : position[0] === 6
+  return [
+    ..._getPossibleMoves(
+      board,
+      position,
+      alliance,
+      isDark ? ["South"] : ["North"],
+      ["Walk"],
+      isIdle ? 2 : 1
+    ),
+    ..._getPossibleMoves(
+      board,
+      position,
+      alliance,
+      isDark
+        ? ["South > East", "South > West"]
+        : ["North > East", "North > West"],
+      ["Attack"],
+      1
+    ),
+  ]
+}
+
+function getPossibleMovesForKing(
+  board: Board,
+  position: Position,
+  alliance: Alliance
+) {
+  return getPossibleMovesForQueen(board, position, alliance, 1)
+}
+
+function getPossibleMovesForQueen(
+  board: Board,
+  position: Position,
+  alliance: Alliance,
+  extent: number = Number.POSITIVE_INFINITY
+) {
+  return [
+    ...getPossibleMovesForRook(board, position, alliance, extent),
+    ...getPossibleMovesForBishop(board, position, alliance, extent),
+  ]
+}
+
+function getPossibleMovesForRook(
+  board: Board,
+  position: Position,
+  alliance: Alliance,
+  extent: number = Number.POSITIVE_INFINITY
+) {
+  return _getPossibleMoves(
+    board,
+    position,
+    alliance,
+    ["North", "East", "South", "West"],
+    ["Attack", "Walk"],
+    extent
+  )
+}
+
+function getPossibleMovesForBishop(
+  board: Board,
+  position: Position,
+  alliance: Alliance,
+  extent: number = Number.POSITIVE_INFINITY
+) {
+  return _getPossibleMoves(
+    board,
+    position,
+    alliance,
+    ["North > East", "North > West", "South > East", "South > West"],
+    ["Attack", "Walk"],
+    extent
+  )
+}
+
+function getPossibleMovesForKnight(
+  board: Board,
+  position: Position,
+  alliance: Alliance
+) {
+  return _getPossibleMoves(
+    board,
+    position,
+    alliance,
+    [
+      "North > East > East",
+      "North > North > East",
+      "North > North > West",
+      "North > West > West",
+      "South > East > East",
+      "South > South > East",
+      "South > South > West",
+      "South > West > West",
+    ],
+    ["Attack", "Walk"],
+    1
+  )
 }
 
 export function _getPossibleMoves(
