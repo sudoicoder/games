6f282cf202544b8b200e467f5ec85d224627ad61
sudoicoder <sudoicoder@gmail.com>
2023-10-09 14:22:43 +0530

    getOffset: returns cached offsets

--- src/chess/services/getOffset.ts
+++ src/chess/services/getOffset.ts
@@ -1,13 +1,22 @@
-export default function getOffset(...directions: Direction[]) {
-  return directions.reduce(
-    (offset, direction) => offset + offsets[direction],
-    0
-  )
+export default function getOffset(direction: Direction) {
+  return offsets[direction]
 }
 
 const offsets: Record<Direction, Offset> = {
-  EAST: +1,
-  NORTH: -1,
-  SOUTH: +1,
-  WEST: -1,
+  "NORTH > EAST > EAST": [-1, +2],
+  "NORTH > EAST > NORTH": [-2, +1],
+  "NORTH > EAST": [-1, +1],
+  "NORTH > WEST > NORTH": [-2, -1],
+  "NORTH > WEST > WEST": [-1, -2],
+  "NORTH > WEST": [-1, -1],
+  "SOUTH > EAST > EAST": [+1, +2],
+  "SOUTH > EAST > SOUTH": [+2, +1],
+  "SOUTH > EAST": [+1, +1],
+  "SOUTH > WEST > SOUTH": [+2, -1],
+  "SOUTH > WEST > WEST": [+1, -2],
+  "SOUTH > WEST": [+1, -1],
+  EAST: [0, +1],
+  NORTH: [-1, 0],
+  SOUTH: [+1, 0],
+  WEST: [0, -1],
 }
