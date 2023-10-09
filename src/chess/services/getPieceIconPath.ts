dd0f3b2bbd31714b3dd9b9aacfc793d16091b7f9
sudoicoder <sudoicoder@gmail.com>
2023-10-09 13:19:00 +0530

    getPieceIconPath: corrected typo

--- src/chess/services/getPieceIconPath.ts
+++ src/chess/services/getPieceIconPath.ts
@@ -1,3 +1,3 @@
 export default function getPieceIconPath(piece: Piece) {
-  return `/games/src/chess/assests/icons/${piece.alliance}-${piece.type}.svg`.toLowerCase()
+  return `/games/src/chess/assets/icons/${piece.alliance}-${piece.type}.svg`.toLowerCase()
 }
