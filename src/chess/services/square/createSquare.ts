993bb302f8754fa48d8c53c640a3822870e4ff15
sudoicoder <sudoicoder@gmail.com>
2023-10-12 20:23:20 +0530

    createSquare: Takes piece as parameter

--- src/chess/services/square/createSquare.ts
+++ src/chess/services/square/createSquare.ts
@@ -1,14 +1,17 @@
 import type Square from "./types/Square"
+import type Piece from "../piece/types/Piece"
 
-import createInitialPiece from "../piece/createInitialPiece"
 import getSquareNotation from "./getSquareNotation"
 import getSquareShade from "./getSquareShade"
 
-export default function createSquare(position: Square["position"]): Square {
+export default function createSquare(
+  position: Square["position"],
+  piece: Nullish<Piece>
+): Square {
   return {
     position,
     notation: getSquareNotation(position),
     shade: getSquareShade(position),
-    piece: createInitialPiece(position),
+    piece,
   }
 }
