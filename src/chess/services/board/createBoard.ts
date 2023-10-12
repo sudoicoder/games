b7c43716fb241b2c78ec1fc3e0d29f87a6724276
sudoicoder <sudoicoder@gmail.com>
2023-10-12 20:25:13 +0530

    createBoard: creates and passes initial pieces to createSquare

--- src/chess/services/board/createBoard.ts
+++ src/chess/services/board/createBoard.ts
@@ -2,13 +2,16 @@ import type Square from "../square/types/Square"
 import type Board from "./types/Board"
 
 import createSquare from "../square/createSquare"
+import createInitialPiece from "../piece/createInitialPiece"
 
 export default function createBoard(): Board {
   const squares = Array<Square[]>(8)
   for (let row = 0; row < squares.length; row++) {
     squares[row] = Array<Square>(8)
     for (let column = 0; column < squares[row].length; column++) {
-      squares[row][column] = createSquare({ row, column })
+      const position = { row, column }
+      const piece = createInitialPiece(position)
+      squares[row][column] = createSquare(position, piece)
     }
   }
   return squares
