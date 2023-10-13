8231a3753b19954c9bf1eaa878da05eb80731b28
sudoicoder <sudoicoder@gmail.com>
2023-10-14 00:02:33 +0530

    usePossibleMoves: type annoted

--- src/chess/hooks/usePossibleMoves.ts
+++ src/chess/hooks/usePossibleMoves.ts
@@ -2,6 +2,7 @@ import { useMemo } from "react"
 
 import type Board from "../services/board/types/Board"
 import type Influence from "../services/influence/types/Influence"
+import type PossibleMoves from "../services/move/types/PossibleMoves"
 import type Piece from "../services/piece/types/Piece"
 
 import computePossibleMoves from "../services/move/computePossibleMoves"
@@ -10,7 +11,7 @@ export default function usePossibleMoves(
   board: Board,
   selectedPiece: Nullish<Piece>,
   opponentInfluence: Influence
-): ReturnType<typeof computePossibleMoves> {
+): PossibleMoves {
   return useMemo(() => {
     if (selectedPiece === null) {
       return new Map()
