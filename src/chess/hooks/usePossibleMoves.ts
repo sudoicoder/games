e043e5d4b0d4a8f37f592795e1d61f9f677448b2
sudoicoder <sudoicoder@gmail.com>
2023-10-13 23:51:47 +0530

    usePossibleMoves: depends on Piece

--- src/chess/hooks/usePossibleMoves.ts
+++ src/chess/hooks/usePossibleMoves.ts
@@ -2,20 +2,19 @@ import { useMemo } from "react"
 
 import type Board from "../services/board/types/Board"
 import type Influence from "../services/influence/types/Influence"
-import type Square from "../services/square/types/Square"
+import type Piece from "../services/piece/types/Piece"
 
 import computePossibleMoves from "../services/move/computePossibleMoves"
 
 export default function usePossibleMoves(
   board: Board,
-  selectedSquare: Nullish<Square>,
+  selectedPiece: Nullish<Piece>,
   opponentInfluence: Influence
 ): ReturnType<typeof computePossibleMoves> {
-  return useMemo(
-    () =>
-      selectedSquare !== null
-        ? computePossibleMoves(board, selectedSquare, opponentInfluence)
-        : [],
-    [board, selectedSquare]
-  )
+  return useMemo(() => {
+    if (selectedPiece === null) {
+      return new Map()
+    }
+    return computePossibleMoves(board, selectedPiece, opponentInfluence)
+  }, [board, selectedPiece])
 }
