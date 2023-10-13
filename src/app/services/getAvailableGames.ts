74390c8ffa230ac1da9523673f452eeefd6f9aa3
sudoicoder <sudoicoder@gmail.com>
2023-10-14 02:12:26 +0530

    getAvailableGames: removed console.log

--- src/app/services/getAvailableGames.ts
+++ src/app/services/getAvailableGames.ts
@@ -4,8 +4,6 @@ import ChessGame from "@/chess/ChessGame"
 import CountryCapitalGame from "@/country-capital/CountryCapitalGame"
 import TicTacToeGame from "@/tic-tac-toe/TicTacToeGame"
 
-console.log(ChessGame.name)
-
 export default function getAvailableGames() {
   return availableGames
 }
