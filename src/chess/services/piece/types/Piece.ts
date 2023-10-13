e786e233ad3afd91d130f8120faca8b4000b90cc
sudoicoder <sudoicoder@gmail.com>
2023-10-13 20:38:44 +0530

    Piece: added square property and made other properties except moves as readonly

--- src/chess/services/piece/types/Piece.ts
+++ src/chess/services/piece/types/Piece.ts
@@ -1,8 +1,12 @@
-type Piece = {
+import type Square from "../../square/types/Square"
+
+type Piece = Readonly<{
   alliance: "dark" | "light"
   notation: string
   designation: "bishop" | "king" | "knight" | "pawn" | "queen" | "rook"
+}> & {
   moves: number
+  square: Nullish<Square>
 }
 
 export default Piece
