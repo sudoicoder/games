654d87e4ce362898062585a4a9ef2eebcb2592c7
sudoicoder <sudoicoder@gmail.com>
2023-10-13 20:41:21 +0530

    createBoard: keeps track of pieces

--- src/chess/services/board/createBoard.ts
+++ src/chess/services/board/createBoard.ts
@@ -1,18 +1,23 @@
+import type Piece from "../piece/types/Piece"
 import type Square from "../square/types/Square"
 import type Board from "./types/Board"
 
-import createSquare from "../square/createSquare"
 import createInitialPiece from "../piece/createInitialPiece"
+import createSquare from "../square/createSquare"
 
 export default function createBoard(): Board {
-  const squares = Array<Square[]>(8)
+  const squares = new Array<Square[]>(8)
+  const pieces = new Set<Piece>()
   for (let row = 0; row < squares.length; row++) {
-    squares[row] = Array<Square>(8)
+    squares[row] = new Array<Square>(8)
     for (let column = 0; column < squares[row].length; column++) {
       const position = { row, column }
       const piece = createInitialPiece(position)
       squares[row][column] = createSquare(position, piece)
+      if (piece !== null) {
+        pieces.add(piece)
+      }
     }
   }
-  return squares
+  return { squares, pieces }
 }
