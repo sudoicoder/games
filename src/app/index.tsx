d7f27e63e9b91001c188d115cf315ca2a2c79e36
sudoicoder <sudoicoder@gmail.com>
2023-10-14 02:11:24 +0530

    App: uses new useApp

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,20 +3,25 @@ import classes from "./styles/app.module.css"
 import useApp from "./hooks/useApp"
 
 export default function App() {
-  const { SelectedGame, getAvailableGames, selectGame } = useApp()
+  const {
+    SelectedGame,
+    getAvailableGameNames,
+    selectGameName,
+    selectedGameName,
+  } = useApp()
   return (
     <div className={classes["wrapper"]}>
       <select
         className={classes["select"]}
-        value={SelectedGame.name}
+        value={selectedGameName}
+        onChange={e => selectGameName(e.target.value)}
       >
-        {getAvailableGames().map(Game => (
+        {getAvailableGameNames().map(gameName => (
           <option
-            key={Game.name}
+            key={gameName}
             className={classes["option"]}
-            label={Game.name.split(/(?=[A-Z])/g).join(" ")}
-            value={Game.name}
-            onClick={() => selectGame(Game)}
+            label={gameName.split(/(?=[A-Z])/g).join(" ")}
+            value={gameName}
           />
         ))}
       </select>
