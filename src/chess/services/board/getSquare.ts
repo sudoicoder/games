a7ffbcd091ebe7018511e6b52ef30559d734568d
sudoicoder <sudoicoder@gmail.com>
2023-10-13 21:03:57 +0530

    deleted getSquare and getSquares

--- src/chess/services/board/getSquare.ts
+++
@@ -1,9 +0,0 @@
-import type Square from "../square/types/Square"
-import type Board from "./types/Board"
-
-export default function getSquare(
-  board: Board,
-  position: Square["position"]
-): Optional<Square> {
-  return board[position.row]?.[position.column]
-}
