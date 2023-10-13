6cff9de15041240546e9bb76df57440e8deb10a0
sudoicoder <sudoicoder@gmail.com>
2023-10-14 01:37:04 +0530

    deleted getAvailableGameNames

--- src/app/services/getAvailableGameNames.ts
+++
@@ -1,7 +0,0 @@
-import getAvailableGames from "./getAvailableGames"
-
-export default function getAvailableGameNames() {
-  return availableGameNames
-}
-
-const availableGameNames = Object.keys(getAvailableGames)
