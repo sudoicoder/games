172b9d2ca73647ee06f7ad369be8706b5b8751ce
sudoicoder <sudoicoder@gmail.com>
2023-10-13 21:40:02 +0530

    Influence: simplified property names

--- src/chess/services/influence/types/Influence.ts
+++ src/chess/services/influence/types/Influence.ts
@@ -2,9 +2,9 @@ import type Piece from "../../piece/types/Piece"
 import type Square from "../../square/types/Square"
 
 type Influence = Readonly<{
-  checking: ReadonlyMap<Piece, ReadonlySet<Square>>
-  controlled: ReadonlyMap<Piece, ReadonlySet<Square>>
-  pinned: ReadonlySet<Piece>
+  checks: ReadonlyMap<Piece, ReadonlySet<Square>>
+  controls: ReadonlyMap<Piece, ReadonlySet<Square>>
+  pins: ReadonlySet<Piece>
 }>
 
 export default Influence
