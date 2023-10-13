bd79b061be4e463158a3be21c2a6e9a15bb3f0de
sudoicoder <sudoicoder@gmail.com>
2023-10-14 00:03:03 +0530

    useOpponentInfluence: type annoted

--- src/chess/hooks/useOpponentInfluence.ts
+++ src/chess/hooks/useOpponentInfluence.ts
@@ -1,18 +1,14 @@
 import { useMemo } from "react"
 
 import type Board from "../services/board/types/Board"
+import type Influence from "../services/influence/types/Influence"
 import type Piece from "../services/piece/types/Piece"
-import type Square from "../services/square/types/Square"
 
 import computeInfluence from "../services/influence/computeInfluence"
 
 export default function useOpponentInfluence(
   board: Board,
-  opposition: Piece["alliance"],
-  opponentSquares: Map<Piece["designation"], Set<Square>>
-) {
-  return useMemo(
-    () => computeInfluence(board, opposition, opponentSquares),
-    [board, opposition, opponentSquares]
-  )
+  opposition: Piece["alliance"]
+): Influence {
+  return useMemo(() => computeInfluence(board, opposition), [board, opposition])
 }
