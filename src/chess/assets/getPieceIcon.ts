5a618aa94478f7f88bd5507fb8b5ddc4a3582349
sudoicoder <sudoicoder@gmail.com>
2023-10-14 15:28:05 +0530

    moved getPieceIcon into assets

---
+++ src/chess/assets/getPieceIcon.ts
@@ -0,0 +1,43 @@
+import type Piece from "../services/piece/types/Piece"
+
+import darkBishop from "./dark/bishop.svg"
+import darkKing from "./dark/king.svg"
+import darkKnight from "./dark/knight.svg"
+import darkPawn from "./dark/pawn.svg"
+import darkQueen from "./dark/queen.svg"
+import darkRook from "./dark/rook.svg"
+import lightBishop from "./light/bishop.svg"
+import lightKing from "./light/king.svg"
+import lightKnight from "./light/knight.svg"
+import lightPawn from "./light/pawn.svg"
+import lightQueen from "./light/queen.svg"
+import lightRook from "./light/rook.svg"
+
+export default function getPieceIcon(
+  alliance: Piece["alliance"],
+  designation: Piece["designation"]
+): string {
+  return pieceIcons[alliance][designation]
+}
+
+const pieceIcons: Record<
+  Piece["alliance"],
+  Record<Piece["designation"], string>
+> = {
+  dark: {
+    bishop: darkBishop,
+    king: darkKing,
+    knight: darkKnight,
+    pawn: darkPawn,
+    queen: darkQueen,
+    rook: darkRook,
+  },
+  light: {
+    bishop: lightBishop,
+    king: lightKing,
+    knight: lightKnight,
+    pawn: lightPawn,
+    queen: lightQueen,
+    rook: lightRook,
+  },
+}
