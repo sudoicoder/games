0f5aced95a927f2f963e325c83a582962b0e2e46
sudoicoder <sudoicoder@gmail.com>
2023-10-14 14:27:38 +0530

    extracted out useStrike

---
+++ src/tic-tac-toe/hooks/useStrike.ts
@@ -0,0 +1,15 @@
+import { useMemo } from "react"
+
+import type Grid from "../services/grid/types/Grid"
+import type Marker from "../services/marker/types/Marker"
+import type Strike from "../services/strike/types/Strike"
+
+import getOppositeMarker from "../services/marker/getOppositeMarker"
+import computeStrike from "../services/strike/computeStrike"
+
+export default function useStrike(grid: Grid, marker: Marker): Nullish<Strike> {
+  return useMemo(
+    () => computeStrike(grid, getOppositeMarker(marker)),
+    [grid, marker]
+  )
+}
