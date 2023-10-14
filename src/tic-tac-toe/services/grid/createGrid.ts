8475bbe4f746596f06a9a6558d53fb3d3a61f1ec
sudoicoder <sudoicoder@gmail.com>
2023-10-14 12:47:10 +0530

    renamed createTicTacToeGrid into createGrid and moved into grid

--- src/tic-tac-toe/services/grid/createGrid.ts
+++ src/tic-tac-toe/services/grid/createGrid.ts
@@ -1,8 +1,8 @@
-import type { TicTacToeSymbol } from "./getTicTacToeSymbols"
+import type TicTacToeSymbol from "../getTicTacToeSymbols"
 
 export type TicTacToeGrid = ("" | TicTacToeSymbol)[][]
 
-export default function createTicTacToeGrid(size: number): TicTacToeGrid {
+export default function createGrid(size: number): TicTacToeGrid {
   return Array(size)
     .fill(undefined)
     .map(() => Array(size).fill(""))
