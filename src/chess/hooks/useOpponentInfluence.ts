1686ba6751f62e2278be9ae2dc6801b0cc3984ab
sudoicoder <sudoicoder@gmail.com>
2023-10-14 15:36:30 +0530

    useOpponentInfluence: takes in alliance

--- src/chess/hooks/useOpponentInfluence.ts
+++ src/chess/hooks/useOpponentInfluence.ts
@@ -4,11 +4,15 @@ import type Board from "../services/board/types/Board"
 import type Influence from "../services/influence/types/Influence"
 import type Piece from "../services/piece/types/Piece"
 
+import getOpposition from "../services/alliance/getOpposition"
 import computeInfluence from "../services/influence/computeInfluence"
 
 export default function useOpponentInfluence(
   board: Board,
-  opposition: Piece["alliance"]
+  alliance: Piece["alliance"]
 ): Influence {
-  return useMemo(() => computeInfluence(board, opposition), [board, opposition])
+  return useMemo(
+    () => computeInfluence(board, getOpposition(alliance)),
+    [board, alliance]
+  )
 }
