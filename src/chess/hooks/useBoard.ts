350f7a37c850d2777d9d0eb1fa7f9c23cc023e00
sudoicoder <sudoicoder@gmail.com>
2023-10-14 15:32:37 +0530

    useBoard: delegated to useLazyRef

--- src/chess/hooks/useBoard.ts
+++ src/chess/hooks/useBoard.ts
@@ -1,10 +1,7 @@
-import { useRef } from "react"
-
-import type Board from "../services/board/types/Board"
+import useLazyRef from "@/common/hooks/useLazyRef"
 
 import createBoard from "../services/board/createBoard"
 
 export default function useBoard() {
-  const board = useRef<Board>()
-  return board.current ?? (board.current = createBoard())
+  return useLazyRef(createBoard)
 }
