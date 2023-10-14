7653f24bbce5559db21246c8bb10505a0a0d43c8
sudoicoder <sudoicoder@gmail.com>
2023-10-14 15:04:27 +0530

    useGrid: uses useLazyRef

--- src/tic-tac-toe/hooks/useGrid.ts
+++ src/tic-tac-toe/hooks/useGrid.ts
@@ -1,15 +1,6 @@
-import { useState } from "react"
-
-import type Grid from "../services/grid/types/Grid"
-
+import useLazyRef from "@/common/hooks/useLazyRef"
 import createGrid from "../services/grid/createGrid"
 
 export default function useGrid() {
-  const [_grid, setGrid] = useState<Grid>(() => createGrid(3))
-
-  const grid = () => _grid
-
-  grid.reset = () => setGrid(createGrid(3))
-
-  return grid
+  return useLazyRef(() => createGrid(3))
 }
