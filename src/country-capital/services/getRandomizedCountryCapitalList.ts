37456e980e238b082952ca42aa1b5f63a48603fc
sudoicoder <sudoicoder@gmail.com>
2023-10-14 19:07:03 +0530

    internalized randomize

--- src/country-capital/services/getRandomizedCountryCapitalList.ts
+++ src/country-capital/services/getRandomizedCountryCapitalList.ts
@@ -1,9 +1,11 @@
-import randomize from "../utils/randomize"
-
 import countryCapitalRecord from "./countryCapitalRecord"
 
 const countryCapitalList = Object.entries(countryCapitalRecord)
 
 export default function getRandomizedCountryCapitalList() {
   return countryCapitalList.flat().sort(randomize)
+}
+
+function randomize() {
+  return Math.random() - 0.5
 }
--- src/country-capital/utils/randomize.ts
+++
@@ -1,3 +0,0 @@
-export default function randomize() {
-  return Math.random() - 0.5
-}
