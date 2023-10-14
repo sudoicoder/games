76189c621ba2913d96479176d454690d1f46d505
sudoicoder <sudoicoder@gmail.com>
2023-10-14 17:41:21 +0530

    createBoard: uses new piece.place

--- src/chess/services/board/createBoard.ts
+++ src/chess/services/board/createBoard.ts
@@ -10,15 +10,16 @@ export default function createBoard(): Board {
     dark: new Set<Piece>(),
     light: new Set<Piece>(),
   }
+
   const squares = new Array<Square[]>(8)
   for (let row = 0; row < squares.length; row++) {
     squares[row] = new Array<Square>(8)
     for (let column = 0; column < squares[row].length; column++) {
       const position = { row, column }
       const piece = createInitialPiece(position)
-      const square = createSquare(position, piece)
+      const square = createSquare(position)
       if (piece !== null) {
-        piece.square = square
+        piece.place(square)
         pieces[piece.alliance].add(piece)
       }
       squares[row][column] = square
