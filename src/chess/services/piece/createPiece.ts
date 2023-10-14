fa0680c3b263712e449bdf78f2aa3604a07abf5a
sudoicoder <sudoicoder@gmail.com>
2023-10-14 16:54:24 +0530

    Piece: made designation, moves, square as getters and added designaten, move and stash methods

--- src/chess/services/piece/createPiece.ts
+++ src/chess/services/piece/createPiece.ts
@@ -1,16 +1,64 @@
 import type Piece from "./types/Piece"
 
 import getPieceNotation from "./getPieceNotation"
+import Square from "../square/types/Square"
 
 export default function createPiece(
   alliance: Piece["alliance"],
   designation: Piece["designation"]
 ): Piece {
-  return {
-    alliance,
-    designation,
-    moves: 0,
-    notation: getPieceNotation(alliance, designation),
-    square: null,
+  const notation = getPieceNotation(alliance, designation)
+
+  let moves = 0
+  let square: Nullish<Square> = null
+
+  function designate(d: Piece["designation"]) {
+    designation = d
   }
+
+  function move(
+    to: Square,
+    howShouldMoveChange: "increment/move" | "decrement/move"
+  ) {
+    if (square !== null) {
+      square.vacate()
+    }
+    to.occupy(piece)
+    square = to
+    switch (howShouldMoveChange) {
+      case "increment/move":
+        moves++
+        break
+      case "decrement/move":
+        moves--
+        break
+    }
+  }
+
+  function stash() {
+    if (square === null) {
+      return
+    }
+    square.vacate()
+    square = null
+  }
+
+  const piece = {
+    alliance,
+    notation,
+    get designation() {
+      return designation
+    },
+    get moves() {
+      return moves
+    },
+    get square() {
+      return square
+    },
+    designate,
+    move,
+    stash,
+  }
+
+  return piece
 }
--- src/chess/services/piece/types/Piece.ts
+++ src/chess/services/piece/types/Piece.ts
@@ -3,10 +3,19 @@ import type Square from "../../square/types/Square"
 type Piece = Readonly<{
   alliance: "dark" | "light"
   notation: string
-}> & {
-  designation: "bishop" | "king" | "knight" | "pawn" | "queen" | "rook"
-  moves: number
-  square: Nullish<Square>
-}
+}> &
+  Readonly<{
+    get designation(): "bishop" | "king" | "knight" | "pawn" | "queen" | "rook"
+    get moves(): number
+    get square(): Nullish<Square>
+  }> &
+  Readonly<{
+    designate: (designation: Piece["designation"]) => void
+    move: (
+      to: Square,
+      howShouldMoveChange: "increment/move" | "decrement/move"
+    ) => void
+    stash: () => void
+  }>
 
 export default Piece
