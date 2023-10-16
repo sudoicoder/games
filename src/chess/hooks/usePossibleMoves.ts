1a0d35614efee826d8cc383cc559925952c068ba
sudoicoder <sudoicoder@gmail.com>
2023-10-16 12:03:29 +0530

    usePossibleMoves: reorganised imports

--- src/chess/hooks/usePossibleMoves.ts
+++ src/chess/hooks/usePossibleMoves.ts
@@ -3,9 +3,9 @@ import { useMemo } from "react"
 import type Board from "../services/board/types/Board"
 import type Influence from "../services/influence/types/Influence"
 import type PossibleMoves from "../services/move/types/PossibleMoves"
+import type Piece from "../services/piece/types/Piece"
 
 import computePossibleMoves from "../services/move/computePossibleMoves"
-import Piece from "../services/piece/types/Piece"
 
 export default function usePossibleMoves(
   board: Board,
@@ -14,6 +14,6 @@ export default function usePossibleMoves(
 ): PossibleMoves {
   return useMemo(
     () => computePossibleMoves(board, opponentInfluence, lastMovedPiece),
-    [board, opponentInfluence]
+    [board, opponentInfluence, lastMovedPiece]
   )
 }
