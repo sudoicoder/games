e5bcf2075c9d19c1b76002a3b82aa182662153d6
sudoicoder <sudoicoder@gmail.com>
2023-10-16 09:55:58 +0530

    useBoard: removed resetBoard

--- src/chess/hooks/useBoard.ts
+++ src/chess/hooks/useBoard.ts
@@ -1,19 +1,8 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 import createBoard from "../services/board/createBoard"
 
 export default function useBoard() {
-  const [board, setBoard] = useState(createBoard)
-
-  useEffect(() => board.stash, [board])
-
-  function resetBoard() {
-    board.stash()
-    setBoard(createBoard())
-  }
-
-  return {
-    board,
-    resetBoard,
-  }
+  const [board] = useState(createBoard)
+  return { board }
 }
