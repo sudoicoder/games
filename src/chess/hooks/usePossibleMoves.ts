aeac5b85fda3805f12d7760e5dcbda799247ecf1
sudoicoder <sudoicoder@gmail.com>
2023-10-16 10:31:37 +0530

    usePossibleMoves: uses new computePossibleMoves

--- src/chess/hooks/usePossibleMoves.ts
+++ src/chess/hooks/usePossibleMoves.ts
@@ -3,18 +3,15 @@ import { useMemo } from "react"
 import type Board from "../services/board/types/Board"
 import type Influence from "../services/influence/types/Influence"
 import type PossibleMoves from "../services/move/types/PossibleMoves"
-import type Piece from "../services/piece/types/Piece"
 
 import computePossibleMoves from "../services/move/computePossibleMoves"
 
 export default function usePossibleMoves(
   board: Board,
-  selectedPiece: Nullish<Piece>,
   opponentInfluence: Influence
 ): PossibleMoves {
-  return useMemo(() => {
-    return selectedPiece !== null
-      ? computePossibleMoves(board, selectedPiece, opponentInfluence)
-      : new Map()
-  }, [board, selectedPiece, opponentInfluence])
+  return useMemo(
+    () => computePossibleMoves(board, opponentInfluence),
+    [board, opponentInfluence]
+  )
 }
