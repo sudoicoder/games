b1431571af4029b762d13bf673395a5963c722e2
sudoicoder <sudoicoder@gmail.com>
2023-10-23 18:48:06 +0530

    useAlliance: exports default

--- src/chess/hooks/useAlliance.ts
+++ src/chess/hooks/useAlliance.ts
@@ -2,7 +2,9 @@ import useTurn from "@/common/hooks/useTurn"
 
 import getOpposition from "../services/alliance/getOpposition"
 
-export default function useAlliance() {
+function useAlliance() {
   const [alliance, flipAlliance] = useTurn("light", getOpposition)
   return { alliance, flipAlliance }
 }
+
+export default useAlliance
