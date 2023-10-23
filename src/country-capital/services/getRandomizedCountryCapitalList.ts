c8ea84dbcaed7b8a613865ac32a1b01ced5f74e9
sudoicoder <sudoicoder@gmail.com>
2023-10-23 17:18:49 +0530

    extracted out randomize from getRandomizedCountryCapitalList

--- src/country-capital/services/getRandomizedCountryCapitalList.ts
+++ src/country-capital/services/getRandomizedCountryCapitalList.ts
@@ -1,11 +1,8 @@
+import randomize from "@/common/utils/randomize"
 import countryCapitalRecord from "./countryCapitalRecord"
 
 const countryCapitalList = Object.entries(countryCapitalRecord)
 
 export default function getRandomizedCountryCapitalList() {
   return countryCapitalList.flat().sort(randomize)
-}
-
-function randomize() {
-  return Math.random() - 0.5
 }
