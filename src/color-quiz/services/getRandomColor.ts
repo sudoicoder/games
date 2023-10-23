d54c6de0368377ad103fcf3fed2fe0a18189b42e
sudoicoder <sudoicoder@gmail.com>
2023-10-23 17:14:47 +0530

    getRandomColor: returns a hex

--- src/color-quiz/services/getRandomColor.ts
+++ src/color-quiz/services/getRandomColor.ts
@@ -1,13 +1,11 @@
-export default function getRandomColor() {
-  return new Array(6)
-    .fill("")
-    .map(() => getRandomHexValue())
-    .join("")
-    .padStart(1, "#")
-}
+const HEX = "0123456789ABCDEF"
 
 function getRandomHexValue() {
   return HEX[Math.floor(Math.random() * HEX.length)]
 }
 
-const HEX = "0123456789ABCDEF"
+function getRandomColor() {
+  return new Array(6).fill("").map(getRandomHexValue).join("").padStart(7, "#")
+}
+
+export default getRandomColor
