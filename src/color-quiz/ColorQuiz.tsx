c51bf777e8abf09c8933faf98e992eb72a65039e
sudoicoder <sudoicoder@gmail.com>
2023-10-23 17:49:26 +0530

    extracted out useColorQuiz

--- src/color-quiz/ColorQuiz.tsx
+++ src/color-quiz/ColorQuiz.tsx
@@ -1,22 +1,38 @@
+import useState from "@/common/hooks/useState"
 import classes from "./color-quiz.module.css"
 import useColorQuiz from "./hooks/useColorQuiz"
 
 export default function ColorQuiz() {
-  useColorQuiz()
+  const {
+    checkIfCorrectChoice,
+    choices,
+    colorToGuess,
+    hasCorrectlyIdentified,
+    hasQuizEnded,
+  } = useColorQuiz()
+
   return (
     <div className={classes["game"]}>
       <p className={classes["prompt"]}>Guess the hex value for this color.</p>
-      <div className={classes["color"]} />
+      <div
+        className={classes["color"]}
+        style={{ color: colorToGuess }}
+      />
       <div className={classes["choices"]}>
-        {["#FFFFFF", "#123456", "#333333"].map(choice => (
+        {choices.map(choice => (
           <button
             key={choice}
             className={classes["choice"]}
+            onClick={() => checkIfCorrectChoice(choice)}
+            disabled={hasQuizEnded}
           >
             {choice}
           </button>
         ))}
       </div>
+      {hasCorrectlyIdentified !== undefined && (
+        <p>{hasCorrectlyIdentified ? "Correct!" : "Incorrect!"}</p>
+      )}
     </div>
   )
 }
