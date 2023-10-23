705b2942c9a8c8a0d98ecc472a378d02bc03b287
sudoicoder <sudoicoder@gmail.com>
2023-10-23 16:43:02 +0530

    created ColorQuiz

--- src/app/services/getAvailableGames.ts
+++ src/app/services/getAvailableGames.ts
@@ -1,6 +1,7 @@
 import type Game from "@/app/services/types/Game"
 
 import ChessGame from "@/chess/ChessGame"
+import ColorQuiz from "@/color-quiz/ColorQuiz"
 import CountryCapitalGame from "@/country-capital/CountryCapitalGame"
 import SynonymGame from "@/synonym/SynonymGame"
 import TicTacToeGame from "@/tic-tac-toe/TicTacToeGame"
@@ -11,6 +12,7 @@ export default function getAvailableGames() {
 
 const availableGames: Record<Game["name"], Game> = {
   ChessGame,
+  ColorQuiz,
   CountryCapitalGame,
   SynonymGame,
   TicTacToeGame,
