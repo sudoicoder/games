23c305ea805347fafd4bab26c8cb52a3fd3a44f2
sudoicoder <sudoicoder@gmail.com>
2023-10-23 17:08:18 +0530

    created getRandomColor

--- src/color-quiz/services/getRandomColor.ts
+++ src/color-quiz/services/getRandomColor.ts
@@ -1,3 +1,13 @@
 export default function getRandomColor() {
-  return "#000000"
+  return new Array(6)
+    .fill("")
+    .map(() => getRandomHexValue())
+    .join("")
+    .padStart(1, "#")
 }
+
+function getRandomHexValue() {
+  return HEX[Math.floor(Math.random() * HEX.length)]
+}
+
+const HEX = "0123456789ABCDEF"
