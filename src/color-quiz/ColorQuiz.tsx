c69cec75d3175e357934a56c5a2b397e67555ba3
sudoicoder <sudoicoder@gmail.com>
2023-10-23 17:01:18 +0530

    ColorQuiz: structured and styled

--- src/color-quiz/ColorQuiz.tsx
+++ src/color-quiz/ColorQuiz.tsx
@@ -1,3 +1,20 @@
+import classes from "./color-quiz.module.css"
+
 export default function ColorQuiz() {
-  return <div>Color Quiz</div>
+  return (
+    <div className={classes["game"]}>
+      <p className={classes["prompt"]}>Guess the hex value for this color.</p>
+      <div className={classes["color"]} />
+      <div className={classes["choices"]}>
+        {["#FFFFFF", "#123456", "#333333"].map(choice => (
+          <button
+            key={choice}
+            className={classes["choice"]}
+          >
+            {choice}
+          </button>
+        ))}
+      </div>
+    </div>
+  )
 }
