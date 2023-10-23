81c37fe260b7a01116b8fadf3687d6e6bd4c1d95
sudoicoder <sudoicoder@gmail.com>
2023-10-23 15:01:05 +0530

    SynonymGame: renders input and button

--- src/synonym/SynonymGame.tsx
+++ src/synonym/SynonymGame.tsx
@@ -1,3 +1,15 @@
+import { useState } from "react"
+import classes from "./synonym-game.module.css"
+
 export default function SynonymGame() {
-  return <div>SynonymGame</div>
+  const [word, setWord] = useState("")
+  return (
+    <div className={classes["game"]}>
+      <input
+        value={word}
+        onChange={e => setWord(e.target.value)}
+      />
+      <button>Find Synonyms</button>
+    </div>
+  )
 }
