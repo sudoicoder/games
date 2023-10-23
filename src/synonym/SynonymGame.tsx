41344968141dc56b26d4858d694fd53593297452
sudoicoder <sudoicoder@gmail.com>
2023-10-23 15:03:46 +0530

    SynonymGame: added label

--- src/synonym/SynonymGame.tsx
+++ src/synonym/SynonymGame.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react"
+import { useId, useState } from "react"
 import classes from "./synonym-game.module.css"
 
 export default function SynonymGame() {
   const [word, setWord] = useState("")
+  const id = useId()
   return (
     <div className={classes["game"]}>
       <form>
+        <label htmlFor={`${id}/word`}>Word</label>
         <input
+          id={`${id}/word`}
           value={word}
           onChange={e => setWord(e.target.value)}
         />
